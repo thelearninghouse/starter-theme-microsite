6cd0c17cbee9d710e888c9ce516b0646b87b7da5
Kurt Rank <krank@learninghouse.com>
2018-01-04 15:23:07 -0500

    Reorganize Sass

--- wp-content/themes/tlh-starter-microsite/library/template-tags.php
+++ wp-content/themes/tlh-starter-microsite/library/template-tags.php
@@ -1,16 +1,4 @@
-<?php function fetch_google_fonts () {
-
-  $headings_font = get_field('headings_font', 'options');
-  $body_font = get_field('body_font', 'options');
-
-  if ($headings_font['label'] === $body_font['label']) {
-    $font_url = 'https://fonts.googleapis.com/css?family=' . str_replace(' ', '+', $headings_font['label']) . ':400,700';
-  }
-  else {
-    $font_url = 'https://fonts.googleapis.com/css?family=' . str_replace(' ', '+', $headings_font['label']) . ':400,700|' . str_replace(' ', '+', $body_font['label']) . ':400,700';
-  }
-  echo '<link href="' . $font_url . '" rel="stylesheet">';
-}
+<?php
 
 function tlh_icon ( $name, $class = NULL ) {
   echo '<i class="icon' . ($class ? ' ' . $class : '') . '">';
