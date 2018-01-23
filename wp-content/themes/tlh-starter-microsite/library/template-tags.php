9f4c031c20417fb8950136f9d949d4200beeb0d7
Kurt Rank <krank@learninghouse.com>
2018-01-23 09:05:21 -0500

    Update tlh_icon to add shortcode and default icon

--- wp-content/themes/tlh-starter-microsite/library/template-tags.php
+++ wp-content/themes/tlh-starter-microsite/library/template-tags.php
@@ -1,10 +1,34 @@
 <?php
 
-function tlh_icon ( $name, $class = NULL, $title = NULL ) {
-  echo '<i class="icon' . ($class ? ' ' . $class : '') . '"' . ($title ? ' title="' . $title . '"': '') . '>';
-  include(get_stylesheet_directory() . '/library/images/icons/' . $name . '.svg');
-  echo '</i>';
+// Icon
+function tlh_build_icon ( $name, $class = NULL, $title = NULL ) {
+  $icon_file = get_stylesheet_directory() . '/library/images/icons/' . $name . '.svg';
+
+  if ( ! file_exists( $icon_file ) ) {
+    $icon_file = get_stylesheet_directory() . '/library/images/icons/unknown.svg';
+  }
+
+  $output  = '<i class="icon' . ($class ? ' ' . $class : '') . '"' . ($title ? ' title="' . $title . '"': '') . '>';
+  $output .= file_get_contents($icon_file);
+  $output .= '</i>';
+
+  return $output;
 }
+
+function tlh_icon ( $name, $class = NULL, $title = NULL ) {
+  echo tlh_build_icon( $name, $class, $title );
+}
+
+function tlh_shortcode_icon ( $atts ) {
+  $a = shortcode_atts( array(
+    'name' => 'info-circle',
+    'class' => 'inline',
+    'title' => NULL,
+  ), $atts );
+
+  return tlh_build_icon( $a['name'], $a['class'], $a['title'] );
+}
+add_shortcode( 'icon', 'tlh_shortcode_icon' );
 
 // $data = [['title' => '...', 'content' => '...'], ... ]
 function tlh_accordion ( $data, $allow_multiple = false, $first_open = false ) {
